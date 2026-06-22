bb74633a955156c8c21dd45aa61b734b0e541a7e
LeoGoder <l.goderneaux@ik.me>
2026-06-22 22:57:03 +0200

    change on lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -104,10 +104,20 @@ return {
 	sections = {
 		--function(str) return icons.vim ..
 	  lualine_a = { { "mode", fmt = function(str) return icons.vim .. (str:sub(1, 1) == "V" and "VISUAL" or str) end } },
-	  lualine_b = { {"branch", icon = icons.git} },
-	  lualine_c = { 
+	  lualine_b = {  
+        {
 	    "filetype", 
+        icon_only = true,
+        separator = "",
+        padding = { left = 1, right = 0},
+        },
+        {
 	    "filename",
+        padding = { left = 0, right = 1},
+        },
+    },
+	  lualine_c = { 
+        {"branch", icon = icons.git},
 	    {
 	      "diagnostics",
 	      sources = { "nvim_diagnostic" },
