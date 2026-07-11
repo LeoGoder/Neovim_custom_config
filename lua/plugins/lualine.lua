331e6c084da68e31b9287425c68f627c1b29630b
LeoGoder <l.goderneaux@ik.me>
2026-07-11 18:24:35 +0200

    strange bug

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -105,15 +105,15 @@ return {
 		--function(str) return icons.vim ..
 	  lualine_a = { { "mode", fmt = function(str) return (str:sub(1, 1) == "V" and "VISUAL" or str) end } },
 	  lualine_b = {  
-        {
-	    "filetype", 
-        icon_only = true,
-        separator = "",
-        padding = { left = 1, right = 0},
-        },
+        -- {
+	    -- "filetype", 
+        -- icon_only = false,
+        -- separator = "",
+        -- padding = { left = 1, right = 0},
+        -- },
         {
 	    "filename",
-        padding = { left = 0, right = 1},
+        padding = { left = 1, right = 1},
         },
     },
 	  lualine_c = { 
